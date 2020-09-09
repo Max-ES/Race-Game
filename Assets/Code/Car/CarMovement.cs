84c74b2891bac9ea8145fc33024e162dffdd1cbc
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-09-09 16:30:00 +0200

    Initial commit

---
+++ Assets/Code/Car/CarMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public WheelCollider WheelColliderLeftFront;
+    public WheelCollider WheelColliderRightFront;
+    public WheelCollider WheelColliderLeftBack;
+    public WheelCollider WheelColliderRightBack;
+
+    public Transform wheelLeftFront;
+    public Transform wheelRightFront;
+    public Transform wheelLeftBack;
+    public Transform wheelRightBack;
+
+    public float motorTorque = 100f;
+    public float maxSteer = 20f;
+    
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        WheelColliderLeftBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        WheelColliderRightBack.motorTorque = Input.GetAxis("Vertical") * motorTorque;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = Vector3.zero;
+        var rot = Quaternion.identity;
+
+        WheelColliderLeftFront.GetWorldPose(out pos, out rot);
+        wheelLeftFront.position = pos;
+        wheelLeftFront.rotation = rot;
+        
+        WheelColliderRightFront.GetWorldPose(out pos, out rot);
+        wheelRightFront.position = pos;
+        wheelRightFront.rotation = rot;
+    }
+}
