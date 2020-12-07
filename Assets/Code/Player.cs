6290a449fc956e6f60646f12f137520c14ab2319
Max Ehrlicher-Schmidt <maxes2000@t-online.de>
2020-12-07 20:17:18 +0100

    WIP: timing

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float BestLapTime { get; private set; } = 9999999;
+    public float LastLapTime { get; private set; } = 0;
+    public float CurrentLapTime { get; private set; } = 0;
+    public float CurrentLap = 0;
+
+    private float lapStartTime;
+    private int lastCheckPointPassed = 0;
+
+    private Transform checkpointsParent;
+
+    private int checkpointCount;
+    //private checkpointLayer;
+
+    void Start()
+    {
+        checkpointsParent = GameObject.Find("Checkpoints").transform;
+        checkpointCount = checkpointsParent.childCount;
+        //checkpointLayer = LayerMask.NameToLayer("Checkpoint");
+
+        //CharacterController = GetComponent(<CarController>)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentLapTime = lapStartTime > 0 ? Time.time - lapStartTime : 0;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        print(collider);
+        if (collider.gameObject.name == "0")
+        {
+            if (lastCheckPointPassed == checkpointCount - 1)
+            {
+                lapFinished();
+                newLap();
+            } else if (CurrentLap == 0)
+            {
+                newLap();
+            }
+        } else if (collider.gameObject.name == (lastCheckPointPassed + 1).ToString())
+        {
+            lastCheckPointPassed++;
+        }
+
+        print(lastCheckPointPassed);
+    }
+
+    void newLap()
+    {
+        print("new lap");
+        CurrentLap++;
+        lastCheckPointPassed = 0;
+        lapStartTime = Time.time;
+    }
+
+    void lapFinished()
+    {
+        print("finished lap");
+        LastLapTime = Time.time - lapStartTime;
+        BestLapTime = Mathf.Min(BestLapTime, LastLapTime);
+    }
+}
---
+++ Assets/Code/UIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject UIRacePanel;
+
+    public Text UITextCurrentLap;
+    public Text UITextCurrentTime;
+    public Text UITextLastLap;
+    public Text UITextBestLap;
+
+    public Player UpdateUIForPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UITextCurrentLap.text = $"Runde: {UpdateUIForPlayer.CurrentLap}";
+        UITextCurrentTime.text = $"Zeit: {UpdateUIForPlayer.CurrentLapTime}s";
+        UITextLastLap.text = $"Letzte Rundenzeit: {UpdateUIForPlayer.LastLapTime}s";
+        UITextBestLap.text = $"Bestzeit: {UpdateUIForPlayer.BestLapTime}s";
+    }
+}
